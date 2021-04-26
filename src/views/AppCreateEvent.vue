06a83933c1f4e227bdb80ba3fef3fc112b937421
DavidDuveau <david.duveau@hotmail.fr>
2021-04-26 19:39:02 +0200

    first commit

---
+++ src/views/AppCreateEvent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <v-form>
+            <v-text-field
+            v-model="organizer"
+            label="Organizer name"
+            required
+        ></v-text-field>
+
+        <v-text-field
+            v-model="title"
+            label="Title"
+            required
+        ></v-text-field>
+
+        <v-text-field
+            v-model="place"
+            label="Place"
+            required
+        ></v-text-field>
+        </v-form>
+
+        <p>{{ organizer }}</p>
+        <p>{{ title }}</p>
+        <p>{{ place }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                organizer: '',
+                title: '',
+                place: '',
+            }
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
